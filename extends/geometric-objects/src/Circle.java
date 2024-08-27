b3f4245273a0a870e61e5fa4dfe733c7e9cbd231
HaiLT <bibo051092@gmail.com>
2024-08-27 21:38:28 +0700

    extends

---
+++ extends/geometric-objects/src/Circle.java
@@ -0,0 +1,60 @@
+public class Circle extends Shape {
+    private double radius;
+
+    public Circle() {
+        super();
+        this.radius = 1.0;
+    }
+
+    public Circle(double radius) {
+        super();
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        this.radius = radius;
+        super.color = color;
+        super.filled = filled;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public String getColor() {
+        return super.getColor();
+    }
+
+    @Override
+    public void setColor(String color) {
+        super.setColor(color);
+    }
+
+    @Override
+    public boolean isFilled() {
+        return super.isFilled();
+    }
+
+    @Override
+    public void setFilled(boolean filled) {
+        super.setFilled(filled);
+    }
+
+    public double getArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    public double getPerimeter() {
+        return  2 * Math.PI * radius;
+    }
+
+    @Override
+    public String toString() {
+        return "A Circle with radius=" + getRadius() + ", which is a subclass of " + super.toString();
+    }
+}
